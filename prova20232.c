7d34d63a6fa418de9a5c71234684857226198afa
MarcosVieira <marcosvieira120404@gmail.com>
2024-05-15 19:58:53 -0300

    questao 5 prova20232

--- prova20232.c
+++ prova20232.c
@@ -2,15 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-int countWordsUpper(char *s);
-
-char* lastName(char *s);
-
-int main(void){
-    char nome[] = "  Marcos Paulo    Marinho Vieira  ";
-    printf("\n\n%s", lastName(nome));
-}
-
 
 
 int countWordsUpper(char*s){
@@ -28,20 +19,29 @@ int countWordsUpper(char*s){
 char* lastName(char* s){
     int tamanho = strlen(s);
     int lastSpace;
-    for(lastSpace = tamanho-1; lastSpace >= 0; lastSpace--){
+    if(*(s + tamanho - 1) == ' '){
+        for(lastSpace = tamanho-1; lastSpace >= 0; lastSpace--){
         if(*(s+lastSpace) == ' ' && *(s + lastSpace - 1) != ' '){
             *(s+lastSpace) = '\0';
             break;
         }
+        }
+        int i;
+        for(i = lastSpace; *(s + i) != ' '; i--);
+        return s+i+1;
     }
-    int i;
-    for(i = lastSpace; *(s + i) != ' '; i--);
-    return s+i+1;
+    else{
+        for(lastSpace = tamanho -1; lastSpace >= 0; lastSpace--){
+            if(*(s + lastSpace) == ' '){
+                return s+lastSpace+1;
+            }
+        }
+    }
 }
 
 void printReverso(char** matriz, int tamanho){
     for(int i = tamanho-1; i>= 0; i--){
-        printf(matriz[i]);
+        printf("%s", matriz[i]);
     }
 }
 
@@ -50,4 +50,51 @@ void libera(char ** matriz, int tamanho){
         free(matriz[i]);
     }
     free(matriz);
+}
+
+void formataLastName(char* lastName){
+    int tamanhoLastName = strlen(lastName);
+    int difMaiusculas = 'A' - 'a';
+    for(int i = 1; i< tamanhoLastName; i++){
+        lastName[i] += difMaiusculas;
+    }
+}
+char* formataIniciais(char* nome){
+    int tamanhoNome = strlen(nome);
+    int qtdIniciais = countWordsUpper(nome);
+    char* iniciais = (char*)malloc(qtdIniciais + qtdIniciais + 1);
+    int j = 2;
+    qtdIniciais -= 1;
+    iniciais[0] = ',';
+    iniciais[1] = ' ';
+    for(int i = 0; i < tamanhoNome; i++){
+            if(*(nome + i) >= 'A' && *(nome + i) <= 'Z'){
+            iniciais[j] = *(nome +i);
+            j++;
+            iniciais[j] = '.';
+            j++;
+            qtdIniciais-=1;
+        }
+            if(qtdIniciais==0){
+                break;
+            }
+    iniciais[j] = 0;
+    }
+    return iniciais;
+}
+
+char* formataNome(char*nome){
+    char *iniciais = formataIniciais(nome);
+    char *ultimoNome = lastName(nome);
+    formataLastName(ultimoNome);
+    char* nomeFormatado = strcat(ultimoNome, iniciais);
+    free(iniciais);
+    return nomeFormatado;
+}
+
+int main(void){
+    char nome[] = "  Marcos Paulo Marinho   Vieira   ";
+    char* nomeFormatado = formataNome(nome);
+    printf("%s", nomeFormatado);
+    return 0;
 }
