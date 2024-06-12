04118e7e11917362abc82ea0e9890cb275943039
Marcos Vieira <102772771+MarcosVieira71@users.noreply.github.com>
2024-06-12 09:57:27 -0300

    finalizado o exercicio de simulado da prova com a funcao de liberar o vetor

--- listasEncadeadasStruct.c
+++ listasEncadeadasStruct.c
@@ -1,24 +1,25 @@
+#define _CRT_SECURE_NO_DEPRECATE
 #define ALPHA 26
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-struct clit {
+struct CList {
 	char* nome;
 	int id;
 	float rating;
-	struct clit* proximo;
+	struct CList* proximo;
 };
 
 struct cadastro {
-	struct clit** vetor;
+	struct CList** vetor;
 };
 typedef struct cadastro Cadastro;
-typedef struct clit Clit;
+typedef struct CList CList;
 
 
 Cadastro* criaCadastro(void);
-Clit* insereNo(Clit* head, Clit* no);
+CList* insereNo(CList* head, CList* no);
 int insere(Cadastro* cad, char* nome, int id, float rating);
 void exibeCadastro(Cadastro* cad);
 float averageRating(Cadastro* cad);
@@ -26,24 +27,24 @@ void liberaCadastro(Cadastro* cad);
 
 int main(void) {
 	Cadastro* cad = criaCadastro();
-	insere(cad, "Fiona", 101, 2.0);	
+	insere(cad, "Fiona", 101, 2.0);
 	insere(cad, "Zelda", 102, 5.0);
 	insere(cad, "Ada Lovelace", 103, 4.5);
 	insere(cad, "Fred", 104, 3.0);
 	insere(cad, "Ahsoka", 105, 5.0);
 	insere(cad, "Fani", 106, 4.0);
 	exibeCadastro(cad);
-	printf("\n\n AVERAGE RATING: %f", averageRating(cad));
+	printf("\n\n AVERAGE RATING: %.2f", averageRating(cad));
 	return 0;
 }
 
 
 
 
-Cadastro* criaCadastro(void){
+Cadastro* criaCadastro(void) {
 	Cadastro* cad = (Cadastro*)malloc(sizeof(Cadastro));
 	if (cad == NULL) exit(1);
-	cad->vetor = (Clit**)malloc(sizeof(Clit*) * ALPHA);
+	cad->vetor = (CList**)malloc(sizeof(CList*) * ALPHA);
 	if (cad->vetor == NULL) exit(1);
 	for (int i = 0; i < ALPHA; i++) {
 		cad->vetor[i] = NULL;
@@ -51,17 +52,17 @@ Cadastro* criaCadastro(void){
 	return cad;
 }
 
-Clit* insereNo(Clit* head, Clit* no) {
+CList* insereNo(CList* head, CList* no) {
 	no->proximo = head;
 	return no;
 }
 
 int insere(Cadastro* cad, char* nome, int id, float rating) {
-	Clit* pessoa = (Clit*)malloc(sizeof(Clit));
+	CList* pessoa = (CList*)malloc(sizeof(CList));
 	if (pessoa == NULL) return 0;
 	pessoa->id = id;
 	pessoa->rating = rating;
-	pessoa->nome = (char*)malloc(strlen(nome)+1);
+	pessoa->nome = (char*)malloc(strlen(nome) + 1);
 	if (pessoa->nome == NULL) return 0;
 	strcpy(pessoa->nome, nome);
 	int index = nome[0] - 'A';
@@ -72,7 +73,7 @@ int insere(Cadastro* cad, char* nome, int id, float rating) {
 void exibeCadastro(Cadastro* cad) {
 	for (int i = 0; i < ALPHA; i++) {
 		printf("%c:\n", 'A' + i);
-		for (Clit* p = cad->vetor[i]; p != NULL; p = p->proximo) {
+		for (CList* p = cad->vetor[i]; p != NULL; p = p->proximo) {
 			printf("  %s %d %f \n", p->nome, p->id, p->rating);
 		}
 	}
@@ -82,7 +83,7 @@ float averageRating(Cadastro* cad) {
 	int counter = 0;
 	float somatorio = 0.0f;
 	for (int i = 0; i < ALPHA; i++) {
-		for (Clit* p = cad->vetor[i]; p != NULL; p = p->proximo) {
+		for (CList* p = cad->vetor[i]; p != NULL; p = p->proximo) {
 			somatorio += p->rating;
 			counter += 1;
 		}
@@ -92,8 +93,12 @@ float averageRating(Cadastro* cad) {
 
 void liberaCadastro(Cadastro* cad) {
 	for (int i = 0; i < ALPHA; i++) {
-		for (Clit* p = cad->vetor[i]; p != NULL; p = p->proximo) {
+		for (CList* p = cad->vetor[i]; p != NULL; p = p->proximo) {
+			CList* t = p;
 			free(p->nome);
+			free(p);
+			p = t;
 		}
 	}
+	free(cad);
 }
